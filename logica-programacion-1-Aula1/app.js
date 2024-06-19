f419e9586d167317c4f249f28fdbe2d065708728
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-06-19 18:29:53 -0500

    nuevos cambios

--- logica-programacion-1-Aula1/app.js
+++
@@ -1,8 +0,0 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
-
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
-}
