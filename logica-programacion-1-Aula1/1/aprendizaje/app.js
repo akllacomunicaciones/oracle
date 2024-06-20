5aa590ba878f3ea6437b2cbf18596ec9dc754d10
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-06-20 15:06:53 -0500

    cambio 8

--- logica-programacion-1-Aula1/1/aprendizaje/app.js
+++ logica-programacion-1-Aula1/1/aprendizaje/app.js
@@ -1,11 +1,101 @@
+//es una buena práctica declarar la variable al inicio
+
+let numeroSecreto = 6;
+let numeroUsuario = prompt("dime tú número");
+
+if(numeroSecreto == numeroUsuario)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variables let or var, const
+
+/* let ventas = 100;
+let costosOperativos = 74;
+
+console.log(ventas - costosOperativos)
+ */
+
+
+/* function saludar(){
+    let nombre = prompt("dime tu nombre");
+    alert("hola " + nombre)
+};
+ 
+saludar();
+
+
+function suma(){
+    let numeroPC = 5;
+    let numeroUsuario = prompt("dime un número");
+    alert(numeroPC + numeroUsuario)
+};
+
+suma();
+ */
+
+
+
+
+//alert("hola emprendedores")
+
+//prompt("dejam tu nombre")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 let numeroUsuario = prompt("Ingrese un número");
 console.log(numeroUsuario);
 
-
-
-
-
-
+ */
 
 
 
