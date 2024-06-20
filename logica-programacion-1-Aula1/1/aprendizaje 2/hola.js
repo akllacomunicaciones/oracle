695b4a5c50014bd6d8a42d5c2840dfdadf989690
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-06-20 17:08:06 -0500

    cambio 10

--- logica-programacion-1-Aula1/1/aprendizaje 2/hola.js
+++ logica-programacion-1-Aula1/1/aprendizaje 2/hola.js
@@ -1,1 +1,15 @@
-alert("hola amigos")
+//alert("hola emprendedores")
+
+/* let ventas = 100;
+let costosOperativos = 70;
+
+console.log(100 - 70); */
+
+
+
+function suma(x,y){
+    console.log(x + y)
+};
+
+suma(4,6);
+suma(76, 24);
