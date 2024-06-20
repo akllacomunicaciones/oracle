4ac31ff0a3e7b459f0a73bdf0671d78cc705db77
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-06-19 23:02:57 -0500

    cambio 6

---
+++ logica-programacion-1-Aula1/1/aprendizaje/app.js
@@ -0,0 +1,23 @@
+/* // negocio 
+let ventas = 100;
+let costos = 40;
+
+console.log(ventas - costos);
+ */
+
+/* 
+alert("hola mundo") */
+
+
+function saludar(){
+    let name = prompt("ingrese su nombre");
+    alert("hola " + name);
+}
+saludar();
+
+
+
+let ingresos = 180;
+let costos = 100;
+
+console.log(ingresos - costos);
