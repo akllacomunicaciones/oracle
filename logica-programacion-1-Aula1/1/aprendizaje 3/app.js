aafab83c9858647871a5edaa929d386d7c28270d
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-06-21 16:07:51 -0500

    cambio 13

--- logica-programacion-1-Aula1/1/aprendizaje 3/app.js
+++ logica-programacion-1-Aula1/1/aprendizaje 3/app.js
@@ -1,3 +1,69 @@
+
+//DENTRO DE LAS CONDICIONALES SE COLOCA LAS VARIABLES(numeroUsuario == numeroSecret)
+
+/* if (condicion1) {
+     Bloque de código que se ejecuta si 'condicion1' es verdadera
+} else if (condicion2) {
+     Bloque de código que se ejecuta si 'condicion1' es falsa y 'condicion2' es verdadera
+} else {
+     Bloque de código que se ejecuta si todas las condiciones anteriores son falsas
+}
+ */
+
+
+
+//SIEMPRE DECLAREN VARIABLES AL INICIO PLEASE
+
+let numeroSecreto = 5;
+let numeroUsuario = prompt("Adivina el número secreto");
+let intentos = 1;
+
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Adivina el número secreto");
+
+    if(numeroUsuario == numeroSecreto){
+        alert(`adivinaste y lo hiciste en ${intentos} intentos` ); // usar siempre `` ese tipo de comillas invertidas para colocar variables dentro de un string o alert
+    }
+
+    else{
+        alert("no adivinaste")
+    }
+    intentos = intentos + 1;
+}
+
+
+
+
+
+
+
+
 //while o bucles
 
-alert("hola empresarios")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+
+function saludar(){
+    alert("hola empresarios")
+};
+
+
+ */
