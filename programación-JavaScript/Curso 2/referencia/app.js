32cf70dc2e2a4f1a138821d10477f0cbecf79a2b
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-07-01 14:09:08 -0500

    cambio 17

---
+++ programación-JavaScript/Curso 2/referencia/app.js
@@ -0,0 +1,6 @@
+/* let ventas = 100; // variable primitiva porque es un valor simple */
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hola Mundo';
+
+let subtitulo = document.querySelector('h2'); // document.querySelector es un método
+subtitulo.innerHTML = "aklla es una gran empresa"; // .innerHTML es una propiedad que permite modificar el contenido de un elemento HTML
---
+++ programación-JavaScript/Curso 2/referencia/aprendizaje 4/app.js
@@ -0,0 +1,8 @@
+//alert("hola mundo");
+
+//variables let, var or const
+/* 
+let x = 1000;
+let y = 60;
+
+console.log(x - y); */
