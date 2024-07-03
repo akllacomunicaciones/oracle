95e8d2f2ab77dd841b1f0129ea9d9b1c334f3a0d
akllacomunicaciones <akllacomunicaciones@gmail.com>
2024-07-03 15:01:11 -0500

    cambio 18

--- programación-JavaScript/Curso 2/referencia/app.js
+++ programación-JavaScript/Curso 2/referencia/app.js
@@ -4,3 +4,23 @@ titulo.innerHTML = 'Hola Mundo';
 
 let subtitulo = document.querySelector('h2'); // document.querySelector es un método
 subtitulo.innerHTML = "aklla es una gran empresa"; // .innerHTML es una propiedad que permite modificar el contenido de un elemento HTML
+
+function alertaSaludo(){
+    alert("Hola Mundo");
+}
+
+
+
+//interectuar con el html através de javascript
+let tituloTres = document.querySelector("h5");
+tituloTres.innerHTML = "#amigosempresarios";
+
+
+
+//FUNCIONES
+
+function nuevoJuego(){
+    alert("iniciaste el juego");
+}
+
+/* nuevoJuego(); //invocar la función */
--- programación-JavaScript/Curso 2/referencia/aprendizaje 4/app.js
+++ programación-JavaScript/Curso 2/referencia/aprendizaje 4/app.js
@@ -6,3 +6,31 @@ let x = 1000;
 let y = 60;
 
 console.log(x - y); */
+
+//JUEGO:
+
+let numeroSecreto = Math.floor(Math.random()*10 + 1); //más 1 siempre para evitar decimal con la función TnumeroSecreto escrito así por camelcase
+let numeroUsuario = 0;
+let intentos = 1;
+
+console.log(numeroSecreto); 
+
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Indica un número entre 1 y 10 por favor");
+
+    if (numeroUsuario == numeroSecreto) {
+        alert("Adivinaste el número secreto");
+    } else{
+        alert("No adivinaste el número secreto");
+    
+        intentos++;
+        if (intentos > 2){
+            alert(`llegaste al número máximo de intentos el número secreto era ${numeroSecreto}`);
+        }
+    
+    }
+    
+    
+}
+
+
